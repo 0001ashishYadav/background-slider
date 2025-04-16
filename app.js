8a1c8f2285accac9f482093a0c0d899a441c0c8f
000ashishYadav <000ashishyadav2003@gmail.com>
2025-04-16 21:03:07 +0530

    next and prev functionality added and made responsive for all devices

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-const imgArr = ["", "", "", "", "", "", ""];
+const mainEl = document.querySelector(".main");
+const imgEl = document.querySelector(".img-con img");
+
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+const imgArr = [
+  "./assets/img-1.jpg",
+  "./assets/img-2.jpg",
+  "./assets/img-3.jpeg",
+  "./assets/img-4.jpg",
+  "./assets/img-5.jpg",
+  "./assets/img-6.jpg",
+  "./assets/img-7.jpeg",
+];
+
+let i = 0;
+
+nextBtn.addEventListener("click", () => {
+  i++;
+  setImg();
+});
+prevBtn.addEventListener("click", () => {
+  i--;
+  setImg();
+});
+
+function setImg() {
+  if (i < 0) {
+    i = imgArr.length - 1;
+  }
+
+  if (i > imgArr.length - 1) {
+    i = 0;
+  }
+
+  imgEl.src = imgArr[i];
+  mainEl.style.backgroundImage = `linear-gradient( rgba(0,0,0,0.775), rgba(0,0,0,0.775),rgba(0,0,0,0.775)), url(${imgArr[i]})`;
+}
